93490dc48b0d39bcf4a4f775c9309c40fd747158
aequorsolutions <dev@aequor.solutions>
2024-08-19 21:28:59 -0300

    fix: :lipstick: adjust on create message form 4 mobile

--- workspace/src/components/room/form.tsx
+++ workspace/src/components/room/form.tsx
@@ -33,7 +33,7 @@ export function FormSendMessage() {
         // router.push("/room/" + theme)
     }
     return (
-        <form action={handleSubmit} className="flex flex-col md:flex-row bg-zinc-900 border border-zinc-800 rounded-xl w-full p-2 ring-orange-400 ring-offset-2 ring-offset-zinc-950 focus-within:ring-1">
+        <form action={handleSubmit} className="flex gap-4 flex-col md:flex-row bg-zinc-900 border border-zinc-800 rounded-xl w-full p-2 ring-orange-400 ring-offset-2 ring-offset-zinc-950 focus-within:ring-1">
             <input 
                     type="text"
                     name= "message"
